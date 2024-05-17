4da1707f45f69f3bc4eddfa9742eabdf64fa7e64
Dewald <dewaldfourie0808@gmail.com>
2024-05-17 14:29:33 +0200

    added author model basics

---
+++ models.js/author.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+// DB Schema for creating a new Author
+const authorSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        }
+    },
+    {timestamps: true},
+);
+
