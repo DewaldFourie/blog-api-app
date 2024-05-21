c412a9f976184bcbc50398122273a5eb91d8c849
Dewald <dewaldfourie0808@gmail.com>
2024-05-21 15:29:58 +0200

    added comment model

---
+++ models.js/comment.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+// New DB Schema for creating a new Comment
+const commentSchema = new mongoose.Schema(
+    {
+        author: {
+            type: String,
+            required: true,
+            maxLength: 20,
+        },
+        text: {
+            type: String,
+            required: true,
+            maxLength: 100,
+        },
+        post: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Post',
+            required: true,
+        },
+        likes: {
+            type: Number
+        },
+    }
+)
