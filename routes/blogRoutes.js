34634079eefb731649f6ee5a32c59f601f5f70f4
Dewald <dewaldfourie0808@gmail.com>
2024-06-03 16:09:30 +0200

    updated comments

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -2,12 +2,12 @@ let express = require('express')
 let router = express.Router();
 const blogController = require('../controllers/blogController');
 
+// Routers to GET posts
 router.get('/', blogController.get_posts_list);
-
 router.get('/last/:number', blogController.get_last_post_list);
-
 router.get('/:postid', blogController.get_post);
 
+// Router to POST a comment on a post
 router.post('/:postid/comment', blogController.post_comment);
 
 module.exports = router;
